4eb9d5523ed836d8b45b605952537a6768ec6e97
Nathan Whitaker <nathanwhitaker@cometnet-10-21-9-12.utdallas.edu>
2018-09-30 17:19:07 -0500

    Updating

--- common_server/common.server/src/main/java/cs6343/server/Controller.java
+++ common_server/common.server/src/main/java/cs6343/server/Controller.java
@@ -12,8 +12,33 @@ public class Controller{
     MetadataServer mds;
 
     @RequestMapping("/ls")
-    public String getPath(@RequestParam("path")String path){
+    public String listPath(@RequestParam("path")String path){
         System.out.println(path); 
+        return path;
+    }
+
+    @RequestMapping("/rm")
+    public String removePath(@RequestParam("path")String path){
+        System.out.println(path);
+        return path;
+    }
+
+    @RequestMapping("/chmod")
+    public String changeMode(@RequestParam("path")String path, @RequestParam("mode") String mode){
+        System.out.println(path);
+        System.out.println(mode);
+        return path;
+    }
+
+    @RequestMapping("/touch")
+    public String createFile(@RequestParam("path")String path){
+        System.out.println(path);
+        return path;
+    }
+
+    @RequestMapping("mkdir")
+    public String makeDir(@RequestParam("path")String path){
+        System.out.println(path);
         return path;
     }
 
