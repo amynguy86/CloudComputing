f67e80f588c9d26d615a4b8be2e17d7abc6a5db8
amynguy86 <arab_amyn@live.com>
2018-10-18 00:12:52 -0500

    rmdir,ls,mkdir implemented withot locking

--- common_server/Server/src/main/java/cs6343/centralized/CentralizedStorage.java
+++ common_server/Server/src/main/java/cs6343/centralized/CentralizedStorage.java
@@ -1,96 +1,148 @@
 package cs6343.centralized;
+
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import cs6343.data.Directory;
+import cs6343.data.FileType;
+import cs6343.data.Inode;
+import cs6343.data.MetaData;
 import cs6343.iface.Storage;
 import cs6343.util.Result;
 
 public class CentralizedStorage extends Storage {
 	public static Logger logger = LoggerFactory.getLogger(CentralizedStorage.class);
-    public Directory rootDir = new Directory();
 
-    @Override
-    public Result<String> ls(String path) {
-        String[] components = path.split("/");
-        Directory d = parentDirectory(path);
-        Result<String> result = new Result<>();
-        result.setOperationSuccess(true);
-        if(d == null){
-        	result.setOperationSuccess(false);
-        	result.setOperationReturnMessage("Not found: " + path);
-        }
-        
-        if(d.subdirectories.get(components[components.length-1])!=null){
-        	result.setOperationReturnMessage(d.subdirectories.get(components[components.length-1]).toString());
-        } else if(d.files.get(components[components.length-1])!= null){
-        	result.setOperationReturnMessage(d.files.get(components[components.length-1]).toString());
-        }
-        else {
-        	result.setOperationSuccess(false);
-        	result.setOperationReturnMessage("Not found: " + path);
-        }
-        
-    	return result;
-        
-    }
-
-   
-
-    public Directory parentDirectory(String path){
-        String[] components = path.split("/");
-        Directory current = rootDir;
-        int i = 0;
-        while(current != null && i < components.length-1){
-            current = current.subdirectories.get(components[i]);
-        }
-        return current;
-    }
-
-
+	public CentralizedStorage() {
+		super();
+	}
 
 	@Override
-	public Result<Boolean> add(String path) {
+	public Result<String> ls(String path) {
+		logger.info("Executing Command ls, data: {}", path);
+		Result<String> result = new Result<>();
+		result.setOperationSuccess(false);
+		
+		String[] directories = path.split("/");
+		Inode inode=this.root;
+		
+		for(int i=1;i<directories.length;i++) {
+			String dir=directories[i];
+			
+			inode=inode.getChild(dir);
+			if(inode==null) {
+				result.setOperationReturnMessage("Unable to find DIR:"+dir);
+				return result;
+			}
+		}
+		
+		String resultStr=inode.getChildren().values().stream().map(node->node.toString()).collect(Collectors.joining("\n"));
+		result.setOperationSuccess(true);
+    	result.setOperationReturnMessage(resultStr);
+		return result;
+	}
+
+	public void parentDirectory(String path) {
+		/*
+		 * String[] components = path.split("/"); Directory current = rootDir; int i =
+		 * 0; while(current != null && i < components.length-1){ current =
+		 * current.subdirectories.get(components[i]); } return current;
+		 */
+	}
+
+	@Override
+	public Result<String> add(String path) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	public Result<String> rm(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public Result<String> mkdir(String path) {
+		logger.info("Executing Command MKDIR, data: {}", path);
+		Result<String> result = new Result<>();
+		result.setOperationSuccess(false);
+		
+		String[] directories = path.split("/");
+		Inode inode=this.root;
+		
+		for(int i=1;i<directories.length-1;i++) {
+			String dir=directories[i];
+			
+			inode=inode.getChild(dir);
+			if(inode==null) {
+				result.setOperationReturnMessage("Unable to find DIR:"+dir);
+				return result;
+			}
+		}
+		
+		if(inode.getChild(directories[directories.length-1])!=null) {
+			result.setOperationReturnMessage("Directory already Exists");
+			return result;
+		}
+		
+		Inode newInode = new Inode();
+    	newInode.setName(directories[directories.length-1]);
+    	newInode.setParent(inode);
+    	MetaData metaData = new MetaData();
+    	metaData.setType(FileType.DIRECTORY);
+    	newInode.setMetaData(metaData);
+    	inode.addChild(newInode);
+    	result.setOperationSuccess(true);
+    	result.setOperationReturnMessage("Directory Created Successfull");
+		return result;
+	}
+	
 
 	@Override
-	public Result<Boolean> rm(String path) {
+	public Result<String> rmdir(String path) {
+		logger.info("Executing Command RMDIR, data: {}", path);
+		Result<String> result = new Result<>();
+		result.setOperationSuccess(false);
+		
+		String[] directories = path.split("/");
+		Inode inode=this.root; 
+		for(int i=1;i<directories.length-1;i++) {
+			String dir=directories[i];
+			
+			inode=inode.getChild(dir);
+			if(inode==null) {
+				result.setOperationReturnMessage("Unable to find DIR:"+dir);
+				return result;
+			}
+			
+			//todo else write lock!
+		}
+		
+		inode=inode.getChild(directories[directories.length-1]);
+		if(inode==null) {
+			result.setOperationReturnMessage("Directory does not Exists");
+			return result;
+		}
+		//todo else write lock
+		
+		inode.getParent().getChildren().remove(inode.getName());
+    	result.setOperationSuccess(true);
+    	result.setOperationReturnMessage("Directory Deleted Successfull");
+    	
+    	//todo loop second time and unlock everything
+		return result;
+	}
+
+	@Override
+	public Result<String> mv(String path) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
-
 	@Override
-	public Result<Boolean> mkdir(String path) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-	@Override
-	public Result<Boolean> rmdir(String path) {
-		// TODO Auto-generated method stub
-		logger.info("Executing Command RMDIR, data: {}",path);
-		return null;
-	}
-
-
-
-	@Override
-	public Result<Boolean> mv(String path) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-	@Override
-	public Result<Boolean> chmod(String path) {
+	public Result<String> chmod(String path) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- common_server/Server/src/test/java/server/FlowTest.java
+++ common_server/Server/src/test/java/server/FlowTest.java
@@ -3,16 +3,35 @@ package server;
 import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cs6343.centralized.CentralizedStorage;
 import cs6343.server.Controller;
 
 public class FlowTest {
+	public static Logger logger = LoggerFactory.getLogger(FlowTest.class);
+
 	//This should executing command RMDIR
-	@Test
+	//@Test
 	public void test1() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Controller controller = new Controller();
 		controller.setStorageSolution(new CentralizedStorage());
 		controller.command("rmdir /something/something1");
+	}
+	
+	@Test
+	public void testLS() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Controller controller = new Controller();
+		controller.setStorageSolution(new CentralizedStorage());
+		logger.info(controller.command("mkdir /amin"));
+		logger.info(controller.command("mkdir /amin/ali"));
+		logger.info(controller.command("mkdir /ali"));
+		logger.info(controller.command("ls /amin"));
+		logger.info(controller.command("ls /"));
+		logger.info(controller.command("rmdir /amin/ali"));
+		logger.info(controller.command("ls /amin"));
+		logger.info(controller.command("rmdir /amin"));
+		logger.info(controller.command("ls /"));
 	}
 }
