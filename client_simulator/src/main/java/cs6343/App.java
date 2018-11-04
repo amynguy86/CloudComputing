4020e6969c77688340992ae131f361d4bfe17977
Nathan Whitaker <nathanwhitaker@cometnet-10-21-53-33.utdallas.edu>
2018-11-04 17:09:25 -0600

    Initial file parser

--- client_simulator/src/main/java/cs6343/App.java
+++ client_simulator/src/main/java/cs6343/App.java
@@ -1,7 +1,25 @@
 package cs6343;
 
-public class App{
-    public static void main(String[] args){
-        System.out.println("Congrats you dont suck");
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory
+            .getLogger(App.class);
+
+    public static void main(String[] args) throws Exception{
+        App app = new App();
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        TreeParser p = new TreeParser();
+        FileNode out = p.readFile("full");
+        System.out.println("Done reading file");
     }
 }
